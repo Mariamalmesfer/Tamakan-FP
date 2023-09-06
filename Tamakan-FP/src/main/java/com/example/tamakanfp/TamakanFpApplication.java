dd5106a77ab151d06f8b26977aaf16a53f355146
Mariam Almesfer <65206318+Mariamalmesfer@users.noreply.github.com>
2023-09-06 12:53:11 +0300

    Add files via upload

---
+++ Tamakan-FP/src/main/java/com/example/tamakanfp/TamakanFpApplication.java
@@ -0,0 +1,13 @@
+package com.example.tamakanfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamakanFpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TamakanFpApplication.class, args);
+    }
+
+}
