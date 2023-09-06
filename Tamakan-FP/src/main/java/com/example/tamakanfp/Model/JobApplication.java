9015656339add309e212da2659ab17137f18d115
bashaer310 <bashaer_alhothali@outlook.sa>
2023-09-06 16:27:41 +0300

    Add model for Job & JobSeekerProfile & JobApplication

---
+++ Tamakan-FP/src/main/java/com/example/tamakanfp/Model/JobApplication.java
@@ -0,0 +1,23 @@
+package com.example.tamakanfp.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class JobApplication {
+
+    @Id
+    private Integer id;
+
+    private String status;
+
+
+}
