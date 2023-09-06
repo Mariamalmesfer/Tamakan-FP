b566ab4c2a650cc25bf6b31d867fbd412df9ec6c
Mariam Almesfer <65206318+Mariamalmesfer@users.noreply.github.com>
2023-09-06 19:50:37 +0300

    m

---
+++ Tamakan-FP/src/main/java/com/example/tamakanfp/Model/User.java
@@ -0,0 +1,87 @@
+package com.example.tamakanfp.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//import java.util.Collections;
+
+@Data
+@AllArgsConstructor
+@Entity
+@RequiredArgsConstructor
+//implements UserDetails
+public class User  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "Username  is required")
+    @Size(min=5,message = "The Username have to be more than 5 litters")
+    @Column(unique = true)
+    private String username;
+
+    @NotEmpty(message = "Password is required")
+    @Size(min=6,message = "The password have to be more than 5 Char")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z]).{6,}$" , message = "the password must have at least 1 number, 1 letter (upper or lower case) and min 6 char")
+    private String password;
+
+    @NotEmpty(message = "Email is required")
+    @Email
+    @Column(unique = true)
+    private String email;
+
+
+    private String role;
+
+    //Relations
+    @OneToOne(cascade =CascadeType.ALL,mappedBy = "user")
+    @PrimaryKeyJoinColumn
+    JobSeeker jopSeeker;
+
+
+    @OneToOne(cascade =CascadeType.ALL,mappedBy = "user")
+    @PrimaryKeyJoinColumn
+    JobProvider jopProvider;
+
+
+
+    //Security
+
+
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Collections.singleton(new SimpleGrantedAuthority(this.role));
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+}
